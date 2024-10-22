9f1e68b783faabafdbdd0901f8ce4c2c81ee0c53
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-10-22 23:50:03 +0500

    Sprint6

---
+++ Tyuiu.RaushKN.Sprint6.Task2.V27/FormMain.cs
@@ -0,0 +1,12 @@
+using Tyuiu.RaushKN.Sprint6.Task2.V27.Lib;
+
+namespace Tyuiu.RaushKN.Sprint6.Task2.V27
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
