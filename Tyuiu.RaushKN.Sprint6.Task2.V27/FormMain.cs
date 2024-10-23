6c991b71e605d1947dda65baf9fa6aa9bd1065d8
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-10-23 16:33:25 +0500

    Sprint5

--- Tyuiu.RaushKN.Sprint6.Task2.V27/FormMain.cs
+++ Tyuiu.RaushKN.Sprint6.Task2.V27/FormMain.cs
@@ -8,5 +8,59 @@ namespace Tyuiu.RaushKN.Sprint6.Task2.V27
         {
             InitializeComponent();
         }
+
+        DataService ds = new DataService();
+        private void bettonDone_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int startStep = Convert.ToInt32(textBoxStart_RKN.Text);
+                int stopStep = Convert.ToInt32(textBoxStop_RKN.Text);
+
+                int len = ds.GetMassFunction(startStep, stopStep).Length;
+
+                double[] valueArray;
+                valueArray = new double[len];
+
+                valueArray = ds.GetMassFunction(startStep, stopStep);
+
+                /*his.chartFunction.Titles.Add("График функции sin(x)");
+
+                this.chartFunction.ChartAreas[0].AxisX.Title = "Ось X";
+                this.chartFunction.ChartAreas[0].AxisY.Title = "Ось Y";
+                */
+                for (int i = 0; i <= len - 1; i++)
+                {
+                    this.dataGridViewResult_RKN.Rows.Add(Convert.ToString(startStep), Convert.ToString(valueArray[i]));
+
+                    // this.chartFunction.Series[0].Points.AddXY(startStep, valueArray[i]);
+
+                    startStep++;
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 2 выполнил студент группы АСОиУб-24-1 Рауш Кирилл Николаевич", "Справка");
+        }
+        private void buttonResult_RKN_MouseDown(object sender, MouseEventArgs e)
+        {
+            buttonResult_RKN.BackColor = Color.Blue;
+        }
+
+        private void buttonResult_RKN_MouseEnter(object sender, EventArgs e)
+        {
+            buttonResult_RKN.BackColor = Color.Red;
+        }
+
+        private void buttonResult_RKN_MouseLeave(object sender, EventArgs e)
+        {
+            buttonResult_RKN.BackColor = Color.Green;
+        }
     }
 }
