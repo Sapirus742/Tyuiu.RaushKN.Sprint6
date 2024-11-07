1d9dc668cb1c6b60bd8e9b23068f40c9b45251db
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-11-07 20:28:35 +0500

    Sprint6

---
+++ Tyuiu.RaushKN.Sprint6.Task7.V16/FormMain.cs
@@ -0,0 +1,14 @@
+using Tyuiu.RaushKN.Sprint6.Task7.V16.Lib;
+
+namespace Tyuiu.RaushKN.Sprint6.Task7.V16
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+    }
+}
